dddbdc604c63b3f6f19e35674cd406d9d79e0602
Sheikh Nahian <sheiknahian06@gmail.com>
2026-06-20 22:32:38 +0600

    photo-update

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -56,7 +56,7 @@ const DashboardLayout = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
                         </label>
                         <div className='flex items-center mr-10 gap-3'>
-                            <img className='rounded-full w-10' src={user?.photoURL} alt="profile" />
+                            <img className='rounded-full w-10 h-10' src={user?.photoURL} alt="profile" />
                             <div>
                                 <p className='text-md font-semibold'>{user?.displayName}</p>
                                 <p className='capitalize'>{role}</p>
